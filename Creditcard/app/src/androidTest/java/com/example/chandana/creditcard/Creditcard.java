d5aff6507a943bd058cf8ea4cbd9fe0955d38f3f
chandanaPerumalla <perumalla.chandana@gmail.com>
2015-03-10 00:36:05 +0530

    Credit Card Application - UI design

---
+++ Creditcard/app/src/androidTest/java/com/example/chandana/creditcard/Creditcard.java
@@ -0,0 +1,18 @@
+package com.example.chandana.creditcard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Chandana on 08-03-2015.
+ */
+public class Creditcard extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen);
+
+    }
+}
